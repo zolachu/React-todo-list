83ca5dada8758f937acaf8c00fff1f7f89f17ea9
Zola <zolboo@alumni.stanford.edu>
2022-08-31 03:12:29 -0700

    assignment 3

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -32,6 +32,7 @@ const ExpenseForm = (props) => {
     setEnteredAmount("");
     setEnteredDate("");
   };
+
   return (
     <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
@@ -66,6 +67,9 @@ const ExpenseForm = (props) => {
         </div>
       </div>
       <div className="new-expense__actions">
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
         <button type="submit" onSubmit={submitHandler}>
           Add Expense
         </button>
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,7 +1,9 @@
 import ExpenseForm from "./ExpenseForm";
+import React, { useState } from "react";
 import "./NewExpense.css";
 
 const NewExpense = (props) => {
+  const [showForm, setShowForm] = useState(false);
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
@@ -9,10 +11,28 @@ const NewExpense = (props) => {
     };
 
     props.onAddExpense(expenseData);
+    setShowForm(false);
   };
+
+  const clickButtonHandler = () => {
+    setShowForm(true);
+  };
+  const cancelHandler = () => {
+    setShowForm(false);
+  };
+
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {showForm && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          showForm={showForm}
+          onCancel={cancelHandler}
+        />
+      )}
+      {!showForm && (
+        <button onClick={clickButtonHandler}>Add New Expense</button>
+      )}
     </div>
   );
 };
